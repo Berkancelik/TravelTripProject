26ddb4e12874814f049ad553a977d2d526cf1b5b
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-07 12:39:45 +0300

    Blog Index has been completed

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TravelTripProject.Models.Classes;
 namespace TravelTripProject.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var blogs = c.Blogs.ToList();
+            return View(blogs);
         }
     }
 }
